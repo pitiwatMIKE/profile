385ef948eba2bddcef3640fab69145c1af7ad098
pitiwatMIKE <pitiwat.pr61@gmail.com>
2022-06-28 14:50:49 +0700

    change name experience to Experience

--- src/utils/ItemLink.js
+++ src/utils/ItemLink.js
@@ -2,7 +2,7 @@ export const isActive = ({ isActive }) => (isActive ? "active" : "");
 
 export const itemLink = [
   { link: "/", name: "About Me" },
-  { link: "/experience", name: "experience" },
+  { link: "/experience", name: "Experience" },
   { link: "/project_detail", name: "Project Detail" },
   { link: "/contact", name: "Contact" },
 ];
